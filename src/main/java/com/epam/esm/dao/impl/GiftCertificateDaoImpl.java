89c53e91aae0afa89bdd9cd828cd10cc9854e517
NodirUmarov <mr.umarov.1995@gmail.com>
2022-06-08 14:01:28 +0600

    Create tables by using reflection api

--- src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
+++ src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
@@ -1,6 +1,7 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.GiftCertificateDao;
+import com.epam.esm.dao.config.DatabaseInitialization;
 import com.epam.esm.model.entity.GiftCertificateEntity;
 import lombok.AccessLevel;
 import lombok.NonNull;
@@ -15,6 +16,11 @@ import org.springframework.stereotype.Repository;
 public class GiftCertificateDaoImpl implements GiftCertificateDao {
 
     @NonNull JdbcTemplate jdbcTemplate;
+    @NonNull DatabaseInitialization databaseInitialization;
+
+    {
+        databaseInitialization.createTable(GiftCertificateEntity.class);
+    }
 
     @Override
     public GiftCertificateEntity findById(Long aLong) {
--- src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
+++ src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
@@ -1,12 +1,26 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.TagDao;
+import com.epam.esm.dao.config.DatabaseInitialization;
 import com.epam.esm.model.entity.TagEntity;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class TagDaoImpl implements TagDao {
 
+    @NonNull JdbcTemplate jdbcTemplate;
+    @NonNull DatabaseInitialization databaseInitialization;
+
+    {
+        databaseInitialization.createTable(TagEntity.class);
+    }
 
     @Override
     public TagEntity findById(Long aLong) {
